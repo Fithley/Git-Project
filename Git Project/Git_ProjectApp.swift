6b71d801e7a973491e6a0e0a09a7827247ca4b6d
Mike Schorah <mike@mikeschorah.com>
2023-08-30 15:45:13 +0100

    Initial Commit

---
+++ Git Project/Git_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ProjectApp.swift
+//  Git Project
+//
+//  Created by Mike Schorah on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Git_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
